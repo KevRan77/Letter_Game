a318c095611748d7e4968ef71daf834bc3b295e7
nremy <nremy@et.esiea.fr>
2017-01-21 22:10:07 +0100

    Commit n°7
    +Ajout classe joueur

---
+++ LetterGame/src/main/java/fr/esiea/Ranaivo_Remy/dictionary/Joueur.java
@@ -0,0 +1,13 @@
+package fr.esiea.Ranaivo_Remy.dictionary;
+
+public class Joueur {
+	String name;
+	
+	public Joueur(){
+		
+	}
+	
+	public String getName(String name){
+		return this.name = name;
+	}
+}
