8b4eaaac638c9e70046b9e76e940c61da13ed36a
KevRan77 <ranaivo@et.esiea.fr>
2017-01-22 18:40:36 +0100

    Commit n°9
    +Nouvelle classe enumération
    +New random avec fréquence d'apparition de voyelles plus élevées
    +Joueur qui commence en premier

--- LetterGame/src/main/java/fr/esiea/Ranaivo_Remy/dictionary/Alphabet.java
+++ LetterGame/src/main/java/fr/esiea/Ranaivo_Remy/dictionary/Alphabet.java
@@ -2,35 +2,74 @@ package fr.esiea.Ranaivo_Remy.dictionary;
 
 import java.util.Random;
 
-public class Alphabet {
-	char letter;
-	//int[] voy = new int[6];
+/*public enum Alphabet {
+	char Alphabet;
+	 	
 	
 		public Alphabet(){
 			Random r = new Random();
-			this.letter = (char)(r.nextInt(26) + 'a');
-			/*int i;
-			switch(this.letter){
-			case 'a' : voy[0]++;
-			break;
-			case 'e' : voy[1]++;
-			break;
-			case 'i' : voy[2]++;
-			break;
-			case 'o' : voy[3]++;
-			break;
-			case 'u' : voy[4]++;
-			break;
-			case 'y' : voy[5]++;
-			break;
-			default : break;
-			}*/
+			this.Alphabet = (char)(r.nextInt(26) + 'a');
 		}
 		
-		public char getLetter(){
-			return this.letter;
+		public char getAlphabet(){
+			return this.Alphabet;
 		}
 		
+}*/
+
+public enum Alphabet
+{
+    A(120,1), B(20,2), C(15,3), D(16,4), E(150,5);/* F(20,6), G(23,7), H(18,8), I(122,9),
+    J(17,10), K(14,11), L(20,12), M(30,13), N(18,14), O(95,15), P(25,16), Q(10,17), R(25,18),
+    S(60,19), T(55,20), U(103,21), V(20,22), W(2,23), X(3,24), Y(4,25), Z(3,26);*/
+    
+    private final int occ;
+    private final int numLetter;
+    
+    private Alphabet(int occ, int numLetter)
+    {
+        this.occ = occ;
+        this.numLetter = numLetter;
+    }
+    
+    public int getValue(){
+    	return numLetter;
+    }
+    
+    private static char[] tab = null;
+    
+    private static void fillTab()
+    {
+        int n = 0;
+        
+        for (Alphabet v : Alphabet.values())
+            n += v.occ;
+        
+        tab = new char[n];
+        
+        int i = 0;
+        for (Alphabet v : Alphabet.values())
+            for (int j = 0; j < v.occ; i++ , j++ )
+                tab[i] = v.toString().charAt(0);
+    }
+    
+    public double getFrequency()
+    {
+        if (tab == null)
+            fillTab();
+        
+        return ((double) occ) / tab.length;
+    }
+    
+    private static Random rand = new Random();
+    
+    public static char random()
+    {
+        if (tab == null)
+            fillTab();
+        
+        return tab[rand.nextInt(tab.length)];
+    }
 }
 
 
--- LetterGame/src/main/java/fr/esiea/Ranaivo_Remy/dictionary/Main.java
+++ LetterGame/src/main/java/fr/esiea/Ranaivo_Remy/dictionary/Main.java
@@ -1,38 +1,97 @@
 package fr.esiea.Ranaivo_Remy.dictionary;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		int nbJoueur;
 		int i;
 		Scanner sc = new Scanner(System.in);
 		do{
 		System.out.print("Veuillez entrer le nombre de joueurs : ");
 		nbJoueur = sc.nextInt();
-		}while(nbJoueur < 1 || nbJoueur > 5);
+		}while(nbJoueur < 2 || nbJoueur > 5);
 		Joueur[] joueur = new Joueur[nbJoueur];
 		String[] a = new String[nbJoueur];
 		Alphabet[] alphabet = new Alphabet[nbJoueur];
+		int[] start = new int[nbJoueur];
 		ArrayList potCommun = new ArrayList();
+		String[] playerName = new String[nbJoueur];
+		
 		
 		for(i = 1; i <= nbJoueur; i++){
 			joueur[i-1] = new Joueur();
 			System.out.println("Entrez le nom du joueur "+i+" : ");
 			a[i-1] = sc.next();
 			
-			alphabet[i-1] = new Alphabet();
+			//alphabet[i-1] = new Alphabet();
+		}
+		char letterChosen;
+		for(i=0; i < nbJoueur; i++){
+			letterChosen = alphabet[i].random();
+			if(letterChosen == 'A'){
+				alphabet[i] = Alphabet.A;
+			}
+			if(letterChosen == 'B'){
+				alphabet[i] = Alphabet.B;
+			}
+			if(letterChosen == 'C'){
+				alphabet[i] = Alphabet.C;
+			}
+			if(letterChosen == 'D'){
+				alphabet[i] = Alphabet.D;
+			}
+			if(letterChosen == 'E'){
+				alphabet[i] = Alphabet.E;
+			}
+			System.out.println("Lettre aléatoire du joueur "+joueur[i].getName(a[i])+" : "+ letterChosen);
+			 potCommun.add(letterChosen); 
+			start[i] = alphabet[i].getValue();
+			playerName[i] = joueur[i].getName(a[i]);
+			// System.out.println("Joueur qui commence : "+alphabet[i]);
+			//System.out.println("Joueur qui commence : "+alphabet[i].getValue());
 		}
 		
-		for(i=0; i < nbJoueur; i++){
-			System.out.println("Lettre aléatoire du joueur "+joueur[i].getName(a[i])+" : "+ alphabet[i].getLetter());
-			potCommun.add(alphabet[i].getLetter());
+		int bubbleInd;
+		String bubblePlayer;
+		for(int j=0; j < nbJoueur; j++){
+		for(i=1; i < nbJoueur; i++){
+			if(start[i-1] > start[i]){
+				bubbleInd = start[i];
+				start[i] = start[i-1];
+				start[i-1] = bubbleInd;
+				
+				bubblePlayer = joueur[i].getName(a[i]);
+				playerName[i] = playerName[i-1];
+				playerName[i-1] = bubblePlayer;
+				
+				}
+			}
 		}
+		System.out.println("Joueur qui commence : "+playerName[0]);
 		System.out.println("Pot commun : "+potCommun);
-	
-	}
+		
+		
+	/*	for (int i = 0; i < 100; i++ )
+            System.out.print(Alphabet.random());*/
 
+	
+		Alphabet test = Alphabet.A;
+		Alphabet test2 = Alphabet.B;
+		if(test.getValue() < test2.getValue()){
+			System.out.println("A<B");
+		}else{
+			System.out.println("A>B");
+		}
+		System.out.println(test.getValue());
+	        
+          
+	}
+		
+		
 }
