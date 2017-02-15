431244bb1c25324dc3c8cf9b1f0014717ad935b9
nremy <nremy@et.esiea.fr>
2017-02-15 21:47:50 +0100

    Commit n°17
    +Classes Tests

---
+++ LetterGame/src/test/java/fr/esiea/Ranaivo_Remy/dictionary/GameTest.java
@@ -0,0 +1,75 @@
+package fr.esiea.Ranaivo_Remy.dictionary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameTest {
+
+	@Test
+	public final void testGame() {
+		fail("Not yet implemented"); // TODO
+	}
+	
+	@Test
+	public final void testGetChoiceNbPlayer() {	
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetNbPlayer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testInitTabPlayer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSetNameEnter() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testFirstDraw() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testPrintMutualBag() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testStartGame() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testWhoStart() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testSortArray() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testMutualBag() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
---
+++ LetterGame/src/test/java/fr/esiea/Ranaivo_Remy/dictionary/PlayerTest.java
@@ -0,0 +1,14 @@
+package fr.esiea.Ranaivo_Remy.dictionary;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public final void testPlayer() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
